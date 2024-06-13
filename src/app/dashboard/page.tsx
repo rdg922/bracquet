5fce93d62dfcb15443ec9265d813a9e53bf8b348
Rohit Dasgupta <rdg922@gmail.com>
2024-06-12 20:50:41 -0400

    Show all tourneys

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,9 @@
-import { getMyTournaments } from "~/server/queries";
+import { getMyTournaments, getTournaments } from "~/server/queries";
 import TournamentCard from "~/components/TournamentCard";
 import { type ITournament } from "~/server/db/schema";
 import AddTournamentForm from "~/components/AddTournament";
 
-async function Tournaments() {
+async function MyTournaments() {
   const tournaments = (await getMyTournaments()) as ITournament[];
   return (
     <div className="grid gap-8 sm:grid-cols-1 lg:grid-cols-3">
@@ -13,10 +13,25 @@ async function Tournaments() {
     </div>
   );
 }
+
+async function AllTournaments() {
+  const tournaments = (await getTournaments()) as ITournament[];
+  return (
+    <div className="grid gap-8 sm:grid-cols-1 lg:grid-cols-3">
+      {tournaments.map((tournament) => (
+        <TournamentCard key={tournament.tournamentId} tournament={tournament} />
+      ))}
+    </div>
+  );
+}
+
 export default async function Dashboard() {
   return (
     <main>
-      <Tournaments />
+      <h2 className="my-6">Your Tournaments</h2>
+      <MyTournaments />
+      <h2 className="my-6">All Tournaments</h2>
+      <AllTournaments />
       <AddTournamentForm />
     </main>
   );
